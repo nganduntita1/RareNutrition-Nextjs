df98f5a5efcffb69d2f1c2d7f24f2fff0685c5b8
Ngandu Ntita <100237894+nganduntita1@users.noreply.github.com>
2025-08-03 14:31:56 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <script defer src="https://static.getbutton.io/widget/bundle.js?id=0qMYZ"></script>
       </head>
       <body
         className={`${dmSans.variable} ${dmSerifDisplay.variable} ${dancingScript.variable} antialiased`}
